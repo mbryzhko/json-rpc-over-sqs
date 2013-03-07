c12fec768fef3ba5f11b32687036a126cd175725
mbryzhko <maxim.bryzhko@gmail.com>
2013-03-07 23:25:05 +0200

    Simple Message Test and Message Perf Test

---
+++ src/test/java/com/bma/amazon/sqs/MessagePerformaceTest.java
@@ -0,0 +1,204 @@
+package com.bma.amazon.sqs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.DeleteMessageRequest;
+import com.amazonaws.services.sqs.model.GetQueueUrlRequest;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.bma.amazon.sqs.MessagePerformaceTest.SqsClient;
+
+public class MessagePerformaceTest {
+	
+	private String queueName;
+	
+	private Set<String> sentMessages;
+	private Set<String> receivedMessages;
+	private BlockingQueue<String> messagesToSend;
+	private AtomicInteger sendersWhichStillRunning = new AtomicInteger(0);
+	
+	private static final int SIZE = 200;
+
+	private List<SqsClient> senders;
+
+	private ArrayList<SqsClient> receivers;
+	
+	
+	@Before
+	public void setUp() {
+		sentMessages = new HashSet<String>();
+		receivedMessages = new HashSet<String>();
+		messagesToSend = new ArrayBlockingQueue(SIZE);
+	
+		givenWeHaveAListOfMessages();
+		
+		messagesToSend.addAll(sentMessages);
+	}
+	
+	private void givenWeHaveAListOfMessages() {
+		System.out.println("Creating a list of " + SIZE + " messages");
+		for (int i = 0; i < SIZE; i++) {
+			sentMessages.add("test_message_" + System.nanoTime());
+		}
+	}
+
+	@After
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	public void verifyPerfomance() throws InterruptedException, IOException, ExecutionException {
+		givenWeHaveAQueueName();
+		givenWeHaveWorkers();
+
+		ExecutorService pool = Executors.newCachedThreadPool();
+		long duration = System.currentTimeMillis();
+		List<Future<Long>> resultSenders = pool.invokeAll(senders);
+		duration = System.currentTimeMillis() - duration;
+		
+		long sum = 0;
+		for (Future<Long> future : resultSenders) {
+			sum += future.get();
+		}
+		System.out.println("Senders: duration: " + duration + ", total spent: " + sum + ", avg: " + sum / SIZE);
+		
+		duration = System.currentTimeMillis();
+		List<Future<Long>> resultsReceivers = pool.invokeAll(receivers);
+		duration = System.currentTimeMillis() - duration;
+		
+		for (Future<Long> future : resultsReceivers) {
+			sum += future.get();
+		}
+		System.out.println("Receiver: duration: " + duration + ", total spent: " + sum + ", avg: " + sum / SIZE);
+		
+		
+		
+		for (SqsClient sqsClient : receivers) {
+			if (sqsClient.isReceive()) {
+				receivedMessages.addAll(sqsClient.getReceived());
+			}
+		}
+		Assert.assertThat(receivedMessages.size(), CoreMatchers.is(sentMessages.size()));
+			
+		
+	}
+
+	private void givenWeHaveWorkers() throws IOException {
+		senders = new ArrayList<SqsClient>();
+		senders.add(new SqsClient(false));
+		senders.add(new SqsClient(false));
+		senders.add(new SqsClient(false));
+		
+		receivers = new ArrayList<SqsClient>();
+		receivers.add(new SqsClient(true));
+		receivers.add(new SqsClient(true));
+		receivers.add(new SqsClient(true));
+		receivers.add(new SqsClient(true));
+	}
+	
+	private void givenWeHaveAQueueName() {
+		queueName = "test_queue_" + System.currentTimeMillis();
+	}
+
+	public class SqsClient implements Callable<Long> {
+		private boolean receive;
+		private AmazonSQSClient sqsClient;
+		private String queueUrl;
+		private List<String> receivedMessages;
+		private long startTime;
+		
+		public SqsClient(boolean receive) throws IOException {
+			this.receive = receive;
+			if (!receive) {
+				sendersWhichStillRunning.intValue();
+			}
+			receivedMessages = new LinkedList<String>();
+			createSqsClient();
+			resolveQueueUrl();
+		}
+
+		private void resolveQueueUrl() {
+			queueUrl = sqsClient.createQueue(new CreateQueueRequest().withQueueName(queueName)).getQueueUrl();
+		}
+
+		private void createSqsClient() throws IOException {
+			InputStream properties = SimpleMessageTest.class.getResourceAsStream("/aws.properties");
+			sqsClient = new AmazonSQSClient(new PropertiesCredentials(properties));
+			sqsClient.setEndpoint("sqs.eu-west-1.amazonaws.com");
+		}
+
+		public Long call() throws Exception {
+			startTime = System.currentTimeMillis();
+			if (receive) {
+				List<Message> messages = null;
+				while (!(messages = sqsClient.receiveMessage(new ReceiveMessageRequest().withQueueUrl(queueUrl)).getMessages()).isEmpty()) {
+					if (!messages.isEmpty()) {
+						for (Message message : messages) {
+							registerReceivedMessage(message);
+							deleteMessageFromAQueue(message);
+						}
+					} else {
+						System.out.println("Received empty messsage");
+					}
+				}
+				System.out.println("Stop reading");
+			} 
+			else {
+				String nextMessageText = null;
+				while((nextMessageText = messagesToSend.poll()) != null) {
+					//System.out.println("Sending message: " + nextMessageText);
+					SendMessageRequest message = new SendMessageRequest().withMessageBody(nextMessageText).withQueueUrl(queueUrl);
+					sqsClient.sendMessage(message);
+				}
+				sendersWhichStillRunning.decrementAndGet();
+			}
+			return System.currentTimeMillis() - startTime;
+		}
+
+		private void registerReceivedMessage(Message message) {
+			receivedMessages.add(message.getBody());
+			//System.out.println("Received message: " + message.getBody());
+		}
+
+		private void deleteMessageFromAQueue(Message message) {
+			sqsClient.deleteMessage(new DeleteMessageRequest().withQueueUrl(queueUrl).withReceiptHandle(message.getReceiptHandle()));
+		}
+
+		public boolean isReceive() {
+			return receive;
+		}
+
+		public List<String> getReceived() {
+			return receivedMessages;
+		}
+	}
+}
---
+++ src/test/java/com/bma/amazon/sqs/SimpleMessageTest.java
@@ -0,0 +1,138 @@
+package com.bma.amazon.sqs;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.CreateQueueResult;
+import com.amazonaws.services.sqs.model.DeleteMessageRequest;
+import com.amazonaws.services.sqs.model.DeleteQueueRequest;
+import com.amazonaws.services.sqs.model.ListQueuesResult;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.amazonaws.services.sqs.model.ReceiveMessageResult;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+
+public class SimpleMessageTest {
+	
+	private AmazonSQSClient sqsClient;
+	private AmazonSQSClient sqsSerive;
+	private String queueName;
+	private String queueUrl;
+
+	@After
+	public void tearDown() {
+		if (queueUrl != null) { 
+			sqsClient.deleteQueue(new DeleteQueueRequest(queueUrl));
+		}
+	}
+	
+	@Test
+	public void verifyThatQueueCanBeCreated() throws IOException {
+		givenWeHaveSqsClient();
+		
+		whenCreateQueue();
+		
+		System.out.println("Created queue url " + queueUrl);
+	}
+
+	private void whenCreateQueue() {
+		queueName = "test_queue_" + System.currentTimeMillis();
+		CreateQueueResult createQueueResult = sqsClient.createQueue(new CreateQueueRequest(queueName));
+		queueUrl = createQueueResult.getQueueUrl();
+	}
+	
+	@Test
+	public void verifyThatMessageCanBeSent() throws Exception {
+		givenWeHaveSqsClient();
+		givenWeHaveSqsService();
+		whenCreateQueue();
+		
+		whenSendAMessage("Hello SQS");
+		
+		thenMessageShouldBeReceived("Hello SQS");
+	}
+	
+	@Test
+	public void verifyThatMessageCanBeDeleted() throws IOException {
+		givenWeHaveSqsClient();
+		givenWeHaveSqsService();
+		whenCreateQueue();
+		
+		whenSendAMessage("Hello SQS");
+		
+		thenMessageIsReceivedAndDeleted();
+		thenNoMessageInTheQueue();
+		
+	}
+	
+	@Test
+	public void verifyThatQueueCanBeDeleted() throws IOException {
+		givenWeHaveSqsClient();
+		List<String> queueUrls = sqsClient.listQueues().getQueueUrls();
+		for (String string : queueUrls) {
+			sqsClient.deleteQueue(new DeleteQueueRequest().withQueueUrl(string));
+		}
+	}
+
+	private void thenNoMessageInTheQueue() {
+		List<Message> messages = receiveMessage();
+		Assert.assertThat(messages.size(), is(0));
+	}
+
+	private void thenMessageIsReceivedAndDeleted() {
+		List<Message> messages = receiveMessage();
+		for (Message message : messages) {
+			deleteMessageFromAQueue(message);
+		}
+	}
+
+	private void thenMessageShouldBeReceived(String expectedText) {
+		List<Message> messages = receiveMessage();
+		
+		assertThat(messages.size(), is(1));
+		Message receivedMessage = messages.get(0);
+		assertThat(receivedMessage.getBody(), is(expectedText));
+		
+		deleteMessageFromAQueue(receivedMessage);
+	}
+
+	private void deleteMessageFromAQueue(Message message) {
+		System.out.println("Deleting message " + message);
+		sqsSerive.deleteMessage(new DeleteMessageRequest(queueUrl, message.getReceiptHandle()));
+	}
+
+	private List<Message> receiveMessage() {
+		ReceiveMessageResult messageResult = sqsSerive.receiveMessage(new ReceiveMessageRequest(queueUrl));
+		System.out.println("Received Message: " + messageResult.toString());
+		List<Message> messages = messageResult.getMessages();
+		return messages;
+	}
+
+	private void givenWeHaveSqsService() throws IOException {
+		InputStream properties = SimpleMessageTest.class.getResourceAsStream("/aws.properties");
+		sqsSerive = new AmazonSQSClient(new PropertiesCredentials(properties));
+		sqsSerive.setEndpoint("sqs.eu-west-1.amazonaws.com");
+	}
+
+	private void whenSendAMessage(String text) {
+		System.out.println("Sending message: " + text + " to queue: " + queueUrl);
+		sqsClient.sendMessage(new SendMessageRequest(queueUrl, text));
+	}
+
+	private void givenWeHaveSqsClient() throws IOException {
+		InputStream properties = SimpleMessageTest.class.getResourceAsStream("/aws.properties");
+		sqsClient = new AmazonSQSClient(new PropertiesCredentials(properties));
+		sqsClient.setEndpoint("sqs.eu-west-1.amazonaws.com");
+	}
+}
